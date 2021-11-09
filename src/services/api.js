0ebf8c1453153b8fbd0602cbe0a35b99605af821
Gian Costa <rafa_gian2011@hotmail.com>
2021-11-09 16:24:36 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,12 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_REST_API_LOCATION
+    baseURL: process.env.REACT_APP_REST_API_LOCATION,
+    withCredentials: false,
+    headers: {
+    'Access-Control-Allow-Origin' : 'https://aircncprod.herokuapp.com/',
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    }
 });
 
 export default api;
